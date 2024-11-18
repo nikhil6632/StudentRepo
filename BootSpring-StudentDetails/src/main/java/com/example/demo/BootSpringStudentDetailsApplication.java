d596bf1a8d43696a52c1af9817a97e5cadce4ef2
nikhi <nikhi@Nikhil>
2024-11-18 22:05:11 +0530

    hi --- Nikhil

--- BootSpring-StudentDetails/src/main/java/com/example/demo/BootSpringStudentDetailsApplication.java
+++ BootSpring-StudentDetails/src/main/java/com/example/demo/BootSpringStudentDetailsApplication.java
@@ -8,6 +8,7 @@ public class BootSpringStudentDetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootSpringStudentDetailsApplication.class, args);
+		System.out.println("Hi This Is Nikhil");
 	}
 
 }
