d56e7d27bb9b81abc18e08218ec8f0e2895a0e49
nikhi <nikhi@Nikhil>
2024-11-18 22:24:00 +0530

    id --- dev

--- BootSpring-StudentDetails/src/main/java/com/example/demo/entity/Student.java
+++ BootSpring-StudentDetails/src/main/java/com/example/demo/entity/Student.java
@@ -1,12 +1,17 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity(name = "StudentInfo")
 public class Student {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer roll;
 	private String name;
 	private Double fee;
